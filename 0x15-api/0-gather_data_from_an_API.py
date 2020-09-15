aae3442c514c5efd24a6ec57e8d08dcfca4a8236
Nicolas Forero <ns.forero@gmail.com>
2020-09-14 22:52:58 -0500

    Update: adding 0 task 0x15

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''fetch info from api'''
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    base_url = 'https://jsonplaceholder.typicode.com/'
+
+    user = '{}users/{}'.format(base_url, sys.argv[1])
+    res = requests.get(user)
+    json_user = res.json()
+    print('Employee {} is done with tasks'.format(
+        json_user.get('name')), end='')
+
+    todos = '{}todos?userId={}'.format(base_url, sys.argv[1])
+    res = requests.get(todos)
+    json_tasks = res.json()
+    done_tasks = []
+
+    for todo in json_tasks:
+        if todo.get('completed') is True:
+            done_tasks.append(todo)
+
+    print('({}/{}):'.format(len(done_tasks), len(json_tasks)))
+    for task in done_tasks:
+        print('\t {}'.format(task.get('title')))
